7f899680d3586bb33bb736be78f0d34db100d798
Ken-muigai <kenmuigai980@gmail.com>
2020-10-12 01:18:41 +0300

    added adapter

--- app/src/main/java/com/kenjoel/letsmeet/ProfileActivity.java
+++ app/src/main/java/com/kenjoel/letsmeet/ProfileActivity.java
@@ -1,6 +1,38 @@
 package com.kenjoel.letsmeet;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ListView;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class ProfileActivity extends AppCompatActivity {
+
+    private List<String> mEverything;
+
+    @BindView(R.id.listView) ListView mListview;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ButterKnife.bind(this);
+
+        Intent intent = getIntent();
+        String name = intent.getStringExtra("name");
+        String email = intent.getStringExtra("email");
+
+        mEverything.add(name);
+        mEverything.add(email);
+
+        ProfileAdapter profileAdapter = new ProfileAdapter(this, android.R.layout.simple_list_item_1, mEverything);
+        mListview.setAdapter(profileAdapter);
+    }
+
 }
--- app/src/main/java/com/kenjoel/letsmeet/ProfileAdapter.java
+++ app/src/main/java/com/kenjoel/letsmeet/ProfileAdapter.java
@@ -1,4 +1,29 @@
 package com.kenjoel.letsmeet;
 
-public class ProfileAdapter {
+import android.content.Context;
+import android.widget.ArrayAdapter;
+
+import java.util.List;
+
+public class ProfileAdapter extends ArrayAdapter {
+
+    private Context mContext;
+    private List<String> mDetails;
+
+    public ProfileAdapter(Context mcontext, int resource, List<String> mdetails){
+        super(mcontext, resource);
+        this.mContext = mcontext;
+        this.mDetails = mdetails;
+    }
+
+    @Override
+    public Object getItem(int position){
+        String mdetails = mDetails.get(position);
+        return mdetails;
+    }
+
+    @Override
+    public int getCount(){
+        return mDetails.size();
+    }
 }
