ae99666304d183d5afbe87351d37ea016cd1fc8e
Ken-muigai <kenmuigai980@gmail.com>
2020-10-11 19:04:40 +0300

    intent

---
+++ app/src/main/java/com/kenjoel/letsmeet/MainActivity.java
@@ -0,0 +1,35 @@
+package com.kenjoel.letsmeet;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @BindView(R.id.creation) Button mCreateAccountButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        mCreateAccountButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void  onClick(View v){
+        if(v == mCreateAccountButton){
+            Intent intent = new Intent(MainActivity.this, SignupActivity.class);
+            startActivity(intent);
+        }
+    }
+}
+
+
+
---
+++ app/src/main/java/com/kenjoel/letsmeet/SignupActivity.java
@@ -0,0 +1,15 @@
+package com.kenjoel.letsmeet;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SignupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.signup);
+    }
+}
