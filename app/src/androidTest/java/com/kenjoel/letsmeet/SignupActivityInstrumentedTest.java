9399a9be79e30ba84ba0b0cb307efcbbee739068
Ken-muigai <kenmuigai980@gmail.com>
2020-10-12 10:58:18 +0300

    WC

--- app/src/androidTest/java/com/kenjoel/letsmeet/SignupActivityInstrumentedTest.java
+++ app/src/androidTest/java/com/kenjoel/letsmeet/SignupActivityInstrumentedTest.java
@@ -8,6 +8,7 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static androidx.test.espresso.Espresso.onData;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
@@ -15,6 +16,7 @@ import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.withChild;
 import static androidx.test.espresso.matcher.ViewMatchers.withClassName;
+import static androidx.test.espresso.matcher.ViewMatchers.withContentDescription;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
@@ -33,17 +35,21 @@ public class SignupActivityInstrumentedTest {
         onView(withId(R.id.name)).perform(typeText("Ken"))
                 .check(matches(withText("Ken")));}
 
-//    @Test
-//    public void theTextsAreReceived(){
-//        String name = "Abdul";
-//        onView(withId(R.id.name)).perform(typeText(name)).perform(closeSoftKeyboard());
-//        try {                             // the sleep method requires to be checked and handled so we use try block
-//            Thread.sleep(250);
-//        } catch (InterruptedException e){
-//            System.out.println("got interrupted!");
-//        }
-//        onView(withId(R.id.launch)).perform(click());
-//        onView(withId(R.id.thisList)).check(matches
-//                (withText(name +" Welcome to Connect, check you email email to activate account ")));
-//    }
+    @Test
+    public void theTextsAreReceived(){
+        String name = "Abdul";
+        String email = "abdulfatah12@gmail.com";
+        onView(withId(R.id.name)).perform(typeText(name)).perform(closeSoftKeyboard());
+        onView(withId(R.id.email)).perform(typeText(email)).perform(closeSoftKeyboard());
+
+        try {                             // the sleep method requires to be checked and handled so we use try block
+            Thread.sleep(250);
+        } catch (InterruptedException e){
+            System.out.println("got interrupted!");
+        }
+        onView(withId(R.id.launch)).perform(click());
+        withContentDescription(R.id.thisList).matches(
+                (withText(name +" Welcome to Connect, check your email " +  email  + " to activate account")));
+        withContentDescription(R.id.password).matches(withText(withId(R.id.confirm).toString()));
+    }
 }
