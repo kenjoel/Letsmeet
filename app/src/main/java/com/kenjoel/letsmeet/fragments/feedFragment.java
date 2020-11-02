a2fe1da44386a3b51b16acd9afc7cc7d61ce5116
Ken-muigai <kenmuigai980@gmail.com>
2020-11-03 01:11:43 +0300

    fragment test 101

--- app/src/main/java/com/kenjoel/letsmeet/fragments/feedFragment.java
+++ app/src/main/java/com/kenjoel/letsmeet/fragments/feedFragment.java
@@ -2,48 +2,61 @@ package com.kenjoel.letsmeet.fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.kenjoel.letsmeet.R;
+import com.kenjoel.letsmeet.adapters.CardsAdapter;
+import com.kenjoel.letsmeet.feed.FeedActivity;
+import com.kenjoel.letsmeet.models.cards;
+import com.lorentzos.flingswipe.SwipeFlingAdapterView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link feedFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class feedFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private static final String TAG = "the Key" ;
+    private CardsAdapter arrayAdapter;
+    private int i;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private String currentUserId;
+    private DatabaseReference Users;
+
+    private FirebaseAuth mAuth;
+
+    private static String userSex;
+    private String oppositeSex;
+    private Object data;
+
+
+
+    List<cards> rowItems;
+
 
     public feedFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment feedFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static feedFragment newInstance(String param1, String param2) {
         feedFragment fragment = new feedFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
     }
@@ -51,16 +64,139 @@ public class feedFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+        Users = FirebaseDatabase.getInstance().getReference().child("Users");
+        getGender();
+
+        mAuth = FirebaseAuth.getInstance();
+        currentUserId = mAuth.getUid();
+
+        rowItems = new ArrayList<>();
+        arrayAdapter = new CardsAdapter(getActivity(), R.layout.item, rowItems);
+
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+
+        SwipeFlingAdapterView flingContainer = (SwipeFlingAdapterView)  getView().findViewById(R.id.frame);
+        flingContainer.setAdapter(arrayAdapter);
+        flingContainer.setFlingListener(new SwipeFlingAdapterView.onFlingListener() {
+            @Override
+            public void removeFirstObjectInAdapter() {
+                // this is the simplest way to delete an object from the Adapter (/AdapterView)
+                Log.d("LIST", "removed object!");
+                rowItems.remove(0);
+                arrayAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onLeftCardExit(Object dataObject) {
+                Toast.makeText(getActivity(), "Nope", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onRightCardExit(Object dataObject) {
+                cards obj = (cards) dataObject;
+                String userId = obj.getUserId();
+                Users.child(currentUserId).child("connections").child(userId).setValue(true);
+                Toast.makeText(getActivity(), "User Followed", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onAdapterAboutToEmpty(int itemsInAdapter) {
+                if(itemsInAdapter == 1){
+                    Toast.makeText(getActivity(), "That's all for today check tomorrow", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onScroll(float scrollProgressPercent) {
+            }
+        });
+
+        flingContainer.setOnItemClickListener(new SwipeFlingAdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClicked(int itemPosition, Object dataObject) {
+                Toast.makeText(getActivity(), "clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_feed, container, false);
     }
+
+    private void getGender() {
+        final FirebaseUser userId = FirebaseAuth.getInstance().getCurrentUser();
+        DatabaseReference mUserDb = Users.child(userId.getUid());
+        mUserDb.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (snapshot.exists()) {
+                    if(snapshot.child("gender") != null){
+                        userSex = snapshot.child("gender").getValue().toString();
+                        switch(userSex){
+                            case "Male":
+                                oppositeSex = "Female";
+                                break;
+                            case "Female":
+                                oppositeSex = "Male";
+                                break;
+                        }
+                        oppositeGenders();
+                    }
+                }
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+
+    public void oppositeGenders(){
+        Users.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                if(snapshot.exists() && !snapshot.child("connections").hasChild(currentUserId) && snapshot.child("gender").getValue().toString().equals(oppositeSex)){
+                    String profileImageUrl = "default";
+
+                    if(!snapshot.child("profileImageUrl").getValue().equals("default")){
+                        profileImageUrl = snapshot.child("profileImageUrl").getValue().toString();
+                    }
+                    cards cards = new cards(snapshot.child("name").getValue().toString(), snapshot.getKey(), profileImageUrl);
+                    rowItems.add(cards);
+                    arrayAdapter.notifyDataSetChanged();
+                }
+
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot snapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+
+        });
+    }
+
+
 }
