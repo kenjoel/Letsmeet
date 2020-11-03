05a2876b1ec385cf4150c6016255591bb84da3a4
Ken-muigai <kenmuigai980@gmail.com>
2020-11-03 05:48:20 +0300

    added icons

--- app/src/main/java/com/kenjoel/letsmeet/adapters/sliderAdapter.java
+++ app/src/main/java/com/kenjoel/letsmeet/adapters/sliderAdapter.java
@@ -1,4 +1,31 @@
 package com.kenjoel.letsmeet.adapters;
 
-public class sliderAdapter {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.viewpager.widget.PagerAdapter;
+
+public class sliderAdapter extends PagerAdapter {
+    Context context;
+    LayoutInflater layoutInflater;
+
+    public sliderAdapter(Context context) {
+        this.context = context;
+    }
+
+    //Arrays for storing the pages
+
+
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
+        return false;
+    }
 }
