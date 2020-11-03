a62cfcb8fa027fede0eeb8f55a8c415e504b1ee4
Ken-muigai <kenmuigai980@gmail.com>
2020-11-03 05:36:31 +0300

    adding slider

---
+++ app/src/main/java/com/kenjoel/letsmeet/fragments/Login.java
@@ -0,0 +1,38 @@
+package com.kenjoel.letsmeet.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.kenjoel.letsmeet.R;
+
+public class Login extends Fragment {
+
+
+    public Login() {
+        // Required empty public constructor
+    }
+
+     public static Login newInstance(String param1, String param2) {
+        Login fragment = new Login();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_login, container, false);
+    }
+}
---
+++ app/src/main/java/com/kenjoel/letsmeet/introActivity.java
@@ -0,0 +1,29 @@
+package com.kenjoel.letsmeet;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import com.kenjoel.letsmeet.fragments.Login;
+import com.kenjoel.letsmeet.fragments.signup;
+
+import java.util.ArrayList;
+
+public class introActivity extends AppCompatActivity {
+
+    private TextView mTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+
+
+    }
+
+}
